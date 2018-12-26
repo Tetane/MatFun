fb619688f72e56bdbfd56573c3ff348691a34800
Tetane <stephane.roussel.fr@gmail.com>
2018-12-26 13:59:30 +0100

    Remove outputsize input parameter. Now the size of the output matrix is automaticaly set by testing the size of the first iteration.

--- matfun.m
+++ matfun.m
@@ -1,16 +1,12 @@
-function [ funmat ] = matfun( fun ,mat, dim, outputsize)
+function [ funmat ] = matfun(fun, mat, dim)
 %MATFUN Apply function to the specified dimension of the matrix
-%   A = MATFUN(FUN, MAT, DIM) applies the function specified by FUN to the dimension DIM of the matrix MAT.
+%   A = MATFUN(FUN, MAT, DIM) applies the function specified by FUN to the dimensions DIM of the matrix MAT.
 %   DIM is vector which maximum size is 3.
-%   A = MATFUN(FUN, MAT, DIM, OUTPUTSIZE) applies the function specified by FUN to the dimension DIM of the matrix MAT.
-%   OUTPUTSIZE specify the size of the output A.
 
 initmatSize = size(mat);
-if nargin == 3
-    outputsize = initmatSize;
-end
 l = length(initmatSize);
 ld = length(dim);
+
 if ld>3
     error('The size of DIM must be between 1 and 3.')
 end
@@ -21,12 +17,12 @@ order(1:ld) = dim;
 tempOrder = initOrder;
 tempOrder(dim) = [];
 order(1+ld : end) = tempOrder;
+
 if ~isequal(order, 1:length(size(mat)))
     mat = permute(mat, order);
 end
 
 matSize = size(mat);
-
 lengthRepVec = prod(matSize(ld+1:end));
 calc = matSize(1:ld);
 
@@ -34,48 +30,32 @@ if l - ld > 1
     mat = reshape(mat, [calc, lengthRepVec]);
 end
 
-testSize = size(fun(mat(:,1)));
-if ~isequal(testSize(1:ld), initmatSize(1:ld)) && nargin == 3
-    warning('The size of the output is different from the size of the input matrix. You should specify the size OUTPUTSIZE of the output matrix.');
-end
-
 switch ld
     case 1
-        if isequal(initmatSize, outputsize)
-            funmat = zeros(matSize(1), lengthRepVec);
-        end
+        testSize = size(fun(mat(:,1)));
+        funmat = zeros(testSize(1), lengthRepVec);
         for ifun = 1:lengthRepVec
             funmat(:,ifun) = fun(mat(:,ifun));
         end
     case 2
-        if isequal(initmatSize, outputsize)
-            funmat = zeros(matSize(1), matSize(2), lengthRepVec);
-        end
+        testSize = size(fun(mat(:,:,1)));
+        funmat = zeros(testSize(1), testSize(2), lengthRepVec);
         for ifun = 1:lengthRepVec
             funmat(:,:,ifun) = fun(mat(:,:,ifun));
         end
     case 3
-        if isequal(initmatSize, outputsize)
-            funmat = zeros(matSize(1), matSize(2), matSize(3), lengthRepVec);
-        end
+        testSize = size(fun(mat(:,:,:,1)));
+        funmat = zeros(testSize(1), testSize(2), testSize(3), lengthRepVec);
         for ifun = 1:lengthRepVec
             funmat(:,:,:,ifun) = fun(mat(:,:,:,ifun));
         end
 end
 
-
 if l - ld > 1
-    try
-        funmat = reshape(funmat, outputsize);
-    catch exception
-        if strcmp(exception.identifier, 'MATLAB:getReshapeDims:notSameNumel') && nargin == 4
-            error('OUTPUTSIZE is not consistent with the actual size of the output.');
-        else
-            error(exception.message);
-        end
-    end
-end 
-
+    funmat = squeeze(reshape(funmat, [testSize(1:ld), matSize(ld+1:end)]));
+else
+    funmat = squeeze(funmat);
+end
 
 end
 
